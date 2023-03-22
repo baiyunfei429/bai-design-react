fa6ace113daa98e9e31523881db1d041e6da3a77
白云飞 <baiyunfei1@xiaomi.com>
2023-03-22 18:45:26 +0800

    feat: 生成 css.js && 按需加载

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const babel = require('gulp-babel');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
+const through2 = require('through2');
 
 const paths = {
   dest: {
@@ -16,16 +17,30 @@ const paths = {
 
 /**
  * 编译脚本文件
- * @param {string} babelEnv babel环境变量
- * @param {string} destDir 目标目录
+ * @param {*} babelEnv babel环境变量
+ * @param {*} destDir 目标目录
  */
 function compileScripts(babelEnv, destDir) {
   const { scripts } = paths;
-  // 设置环境变量
   process.env.BABEL_ENV = babelEnv;
   return gulp
     .src(scripts)
     .pipe(babel()) // 使用gulp-babel处理
+    .pipe(
+      through2.obj(function z(file, encoding, next) {
+        this.push(file.clone());
+        // 找到目标
+        if (file.path.match(/(\/|\\)style(\/|\\)index\.js/)) {
+          const content = file.contents.toString(encoding);
+          file.contents = Buffer.from(cssInjection(content)); // 文件内容处理
+          file.path = file.path.replace(/index\.js/, 'css.js'); // 文件重命名
+          this.push(file); // 新增该文件
+          next();
+        } else {
+          next();
+        }
+      }),
+    )
     .pipe(gulp.dest(destDir));
 }
 
@@ -66,6 +81,19 @@ function less2css() {
     .pipe(cssnano({ zindex: false, reduceIdents: false })) // 压缩
     .pipe(gulp.dest(paths.dest.lib))
     .pipe(gulp.dest(paths.dest.esm));
+}
+
+/**
+ * 当前组件样式 import './index.less' => import './index.css'
+ * 依赖的其他组件样式 import '../test-comp/style' => import '../test-comp/style/css.js'
+ * 依赖的其他组件样式 import '../test-comp/style/index.js' => import '../test-comp/style/css.js'
+ * @param {string} content
+ */
+function cssInjection(content) {
+  return content
+    .replace(/\/style\/?'/g, "/style/css'")
+    .replace(/\/style\/?"/g, '/style/css"')
+    .replace(/\.less/g, '.css');
 }
 
 // 整体并行执行任务
