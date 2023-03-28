420696f9f132ea29774c3be798ca8feb96fd20fb
白云飞 <baiyunfei1@xiaomi.com>
2023-03-28 11:34:05 +0800

    feat: add commitlint,规范commit message

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,1 +1,33 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+/**
+ * 类型	描述
+ * 
+build	编译相关的修改，例如发布版本、对项目构建或者依赖的改动
+chore	其他修改, 比如改变构建流程、或者增加依赖库、工具等
+ci	持续集成修改
+docs	文档修改
+feat	新特性、新功能
+fix	修改bug
+perf	优化相关，比如提升性能、体验
+refactor	代码重构
+revert	回滚到上一个版本
+style	代码格式修改, 注意不是 css 修改
+test	测试用例修改
+
+*/
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional'
+  ],
+  // rules: {
+  //   'type-enum': [2, 'always', [
+  //     'feat', 'update', 'fix', 'refactor', 'optimize', 'style', 'docs', 'chore', 'perf', 'test', 'ci'
+  //   ]],
+  //   'type-case': [0],
+  //   'type-empty': [0],
+  //   'scope-empty': [0],
+  //   'scope-case': [0],
+  //   'subject-full-stop': [0, 'never'],
+  //   'subject-case': [0, 'never'],
+  //   'header-max-length': [0, 'always', 72]
+  // }
+};
